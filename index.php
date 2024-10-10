900006508ac97176b4f7509b5fdeca15e900427f
digos21 <rodrigo.pereira.it2b@gmail.com>
2024-10-10 17:08:50 -0300

    iniciando o projeto

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload csv</title>
+</head>
+<body>
+    <h1>Upload csv e xls</h1>
+    <form action="">
+        <label for="">Arquivo</label>
+        <br>
+        <input type="file">
+        <br>
+        <br>
+        <input type="submit">
+
+    </form>
+</body>
+</html>
